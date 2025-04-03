0fbf668f91af66fbe201ea9e9ddbb4ea8deb6681
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-03 15:43:42 +0200

    add implementation of Card interface

---
+++ src/main/java/it/unibo/model/impl/card/CardImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.model.impl.card;
+
+import it.unibo.model.api.card.Card;
+import it.unibo.model.api.card.Requirement;
+
+/**
+ * the implementation of Card interface.
+ */
+public class CardImpl implements Card {
+    private final String name;
+    private final Requirement requirement;
+
+    /**
+     * @param name the name of this specific card
+     * @param requirement the requirement of this specific card
+     */
+    public CardImpl(final String name, final Requirement requirement) {
+        this.name = name;
+        this.requirement = requirement;
+    }
+
+    @Override
+    public final String getName() {
+       return this.name;
+    }
+
+    @Override
+    public final Requirement getRequirement() {
+        return this.requirement;
+    }
+}
