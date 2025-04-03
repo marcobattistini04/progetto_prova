d3282929ea2ecdce1250a3ea08ee302e3f3605c9
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-03 15:41:34 +0200

    added Requirement to own a specific Card

---
+++ src/main/java/it/unibo/model/api/card/Requirement.java
@@ -0,0 +1,11 @@
+package it.unibo.model.api.card;
+
+/**
+ * the Requirement to own a specific Card.
+ *
+ * @param category the category the requirement is referring to
+ * @param amount the amount of the specific category required.
+ */
+public record Requirement(String category, int amount) {
+
+}
