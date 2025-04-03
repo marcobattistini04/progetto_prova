3e852bb49a4f82ce32efb9dbd1c40b587cf83218
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-03 16:33:51 +0200

    add playable character interface

---
+++ src/main/java/it/unibo/model/api/card/characters/PlayableCharacter.java
@@ -0,0 +1,26 @@
+package it.unibo.model.api.card.characters;
+
+/**
+ * A character that can be played from the player's deck during a match.
+ */
+public interface PlayableCharacter extends Character {
+
+    /**
+     * @return this playable character type (melee or ranged)
+     */
+    String getType();
+    /**
+     * @return total copies of this character playable in one round
+     */
+    int getCopiesPerRound();
+
+    /**
+     * @return amount of energy needed to play this character
+     */
+    int getEnergyToPlay();
+
+    /**
+     * @return this character action range (distance form witch this character can spot enemies) 
+     */
+    int getActionRange();
+}
