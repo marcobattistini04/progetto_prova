07646319fd0af98a48013c77855de6071ac04c8d
Marco Battistini <marco.battistini10@studio.unibo.it>
2025-04-03 15:36:03 +0200

    added Card interface

---
+++ src/main/java/it/unibo/model/api/card/Card.java
@@ -0,0 +1,17 @@
+package it.unibo.model.api.card;
+
+/**
+ * A generic Card of the Collection.
+ */
+public interface Card {
+    /**
+     * @return the card name
+     */
+    String getName();
+
+    /**
+     * @return the requirement to own this card (ex. Coins, 200 -> 200 Coins needed)
+     */
+    Requirement getRequirement();
+}
+
